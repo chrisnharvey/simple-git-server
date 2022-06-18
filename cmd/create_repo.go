eadb61343cb439f3a452847d8d9f47efde2a5c5a
Chris Harvey <chris@chrisnharvey.com>
2022-06-18 23:47:32 +0100

    Add logic to create-repo command

--- cmd/create_repo.go
+++ cmd/create_repo.go
@@ -1,20 +1,69 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
 
 func CreateRepo() *cobra.Command {
 	return &cobra.Command{
-		Use:       "create-repo",
-		Short:     "Create new git repo.",
-		ValidArgs: []string{"a", "b", "c"},
-		Run:       createRepoCommand,
+		Use:   "create-repo [name]",
+		Short: "Create new git repo.",
+		Run:   createRepoCommand,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				return errors.New("name must be provided")
+			}
+
+			return nil
+		},
 	}
 }
 
 func createRepoCommand(cmd *cobra.Command, args []string) {
-	fmt.Println("Hello world")
+	for _, v := range args {
+		if _, err := os.Stat("/repos/" + v); err == nil {
+			fmt.Println(errors.New(v + " already exists"))
+		}
+	}
+
+	for _, v := range args {
+		err := createRepo(v)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(v + " created")
+	}
+}
+
+func createRepo(n string) error {
+	err := runCommand("sudo", "mkdir", "/repos/"+n)
+	if err != nil {
+		return err
+	}
+
+	err = runCommand("sudo", "git", "init", "--bare", "/repos/"+n)
+	if err != nil {
+		return err
+	}
+
+	err = runCommand("sudo", "chown", "git:git", "-R", "/repos/"+n)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func runCommand(c string, args ...string) error {
+	cmd := exec.Command(c, args...)
+
+	return cmd.Run()
 }
